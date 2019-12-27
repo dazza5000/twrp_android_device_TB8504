b48a56b4b322cbabd2e96e79b8106e6c6909626f
dazza5000 <darran.kelinske@gmail.com>
2019-12-27 11:22:40 -0600

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter tb_8504x,$(TARGET_DEVICE)),)
+ifneq ($(filter TB8504,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
